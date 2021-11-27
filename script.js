a6ad7c66fc450121eadc6cd433ff8c7882016547
Dan <80458000+Web-Dev-Dan@users.noreply.github.com>
2021-11-27 22:01:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const resetDisplayFlex = document.getElementById('resetDisplayFlex');
+const submitDisplayFlex = document.getElementById('submitDisplayFlex');
+const inputDisplayFlex = document.getElementById('inputDisplayFlex');
+const displayFlexContainer = document.getElementById('displayFlexContainer');
 
+submitDisplayFlex.addEventListener('click', () => {
+    if (inputDisplayFlex.value == 'display: flex;' || inputDisplayFlex.value == 'display:flex;') {
+        displayFlexContainer.style.display = 'flex';
+        inputDisplayFlex.value = '';
+        inputDisplayFlex.style.border = '.1rem solid gray';
+    }
+    else {
+        inputDisplayFlex.style.border = '.1rem solid red';
+    }
+});
+
+resetDisplayFlex.addEventListener('click', () => {
+    displayFlexContainer.style.display = 'block';
+    inputDisplayFlex.value = '';
+    inputD
